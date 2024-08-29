a91a79d42b9c49b51ac94af6910bad876af77d2e
snake0207 <snake0207@gmail.com>
2024-08-29 17:33:21 +0900

    add dynamic metadata

--- app/(Home)/page.js
+++ app/(Home)/page.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
   title: "Home",
@@ -14,14 +16,15 @@ async function getMovies() {
 export default async function Home() {
   const movies = await getMovies();
   return (
-    <div>
-      <ul style={{ listStyle: "none" }}>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      {movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+        />
+      ))}
     </div>
   );
 }
--- components/movie-videos.js
+++ components/movie-videos.js
@@ -1,10 +1,23 @@
 import { API_URL } from "../app/(Home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id) {
   return await fetch(`${API_URL}/${id}/videos`).then((resp) => resp.json());
 }
 
 export default async function MovieVideos({ id }) {
-  const video = await getVideos(id);
-  return <h6>{JSON.stringify(video)}</h6>;
+  const videos = await getVideos(id);
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.key}
+          src={`https://www.youtube.com/embed/${video.key}`}
+          allowFullScreen
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
