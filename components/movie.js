a2e77181d2903ebef931754b98a11454b5b2d4be
snake0207 <snake0207@gmail.com>
2024-08-29 18:01:50 +0900

    add constant

---
+++ components/movie.js
@@ -0,0 +1,19 @@
+"use client";
+
+import Link from "next/link";
+import styles from "../styles/movie.module.css";
+import { useRouter } from "next/navigation";
+
+export default function Movie({ id, title, poster_path }) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
+
+  return (
+    <div className={styles.movie}>
+      <img src={poster_path} alt={title} onClick={onClick} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+}
